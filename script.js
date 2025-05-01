9679d3d53bd71bcada870ec86c70f05cf6a00891
Athira <ConsulenteApplyScience_Selvan@brembo.it>
2025-05-01 18:19:54 +0200

    images

--- script.js
+++ script.js
@@ -11,6 +11,18 @@ async function fetchProductData() {
         console.error('Error fetching data:', error);
     }
 }
+function showImages(category, subcategory) {
+  fetch(sheetURL)
+      .then(response => response.text())
+      .then(csvText => {
+          const data = csvToJson(csvText);
+          const filtered = data.filter(item => 
+              item['Category'] === category && item['Subcategory'] === subcategory
+          );
+          populateCatalog(filtered);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+}
 
 // Convert CSV to JSON
 function csvToJson(csvText) {
